0631fb6cc478f72ac5440f363c70b17822ddccb9
Ved Dandotia <vednkps@gmail.com>
2025-06-22 22:42:18 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -2,6 +2,6 @@
 
 int main() {
     int a,b;
-    scanf("%d %d" ,&a, &b);
+    scanf("%d %d" ,&a, &b);  //scanf function is used to take input from user fgets is also a input function
     return 0;
 }
